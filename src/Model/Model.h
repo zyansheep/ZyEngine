790f91626bb5bbf66802b2bc19bc470c0e3611f7
Zyansheep <zykersheep@gmail.com>
2019-12-16 08:46:00 -0500

    add Model System initial commit

---
+++ src/Model/Model.h
@@ -0,0 +1,41 @@
+
+
+class ModelGenerator(){
+  static Model IsometricSphere(uint resolution){
+    
+  }
+  static Model Cube(){
+    std::vector<float> VertexBuffer
+    Model m;
+    return Model({
+      
+    },{
+      
+    })
+  }
+  static Model LoadObj(){
+    
+  }
+}
+
+class Model(){
+public:
+  std::vector<float> VertexBuffer;
+  std::vector<uint> IndexBuffer;
+  
+  Model(){
+    
+  }
+  
+  void Load(){
+    
+  }
+  void Unload(){
+    
+  }
+  void GetVertexArray(){
+    return m_VertexArray;
+  }
+private:
+  VertexArray m_VertexArray;
+}

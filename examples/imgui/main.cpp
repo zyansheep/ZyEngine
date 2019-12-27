a3c57f1bc58b1a8e36d85a9f2351660092fb7af5
zyansheep <zyansheep>
2019-12-27 18:39:53 -0500

    add gui example

---
+++ examples/imgui/main.cpp
@@ -0,0 +1,37 @@
+#include "Core/Window.h"
+#include "Core/Gui.h"
+
+//Window (Width, Height, Title)
+Window window = Window(1280, 720, "Examples - Template");
+
+Gui gui = Gui(window);
+
+ImVec4 clearColor = ImVec4(0.5,0.5,1.0,1.0);
+
+void setup(){
+  gui.Init();
+  
+  glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_LESS);
+}
+
+void loop(){
+  glClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  
+  gui.Begin("Hello There~");
+    ImGui::Button("This is a button");
+    ImGuiColorEditFlags misc_flags = (0) | (ImGuiColorEditFlags_NoDragDrop) | (ImGuiColorEditFlags_AlphaPreviewHalf) | (ImGuiColorEditFlags_NoOptions);
+    ImGui::ColorPicker4("##picker", (float*)&clearColor, misc_flags | ImGuiColorEditFlags_NoSidePreview | ImGuiColorEditFlags_NoSmallPreview);
+    ImGui::Text("FrameTime: %f seconds", window.GetFrameTime());
+    ImGui::Text("Mouse Position: (%d, %d)", window.MouseX, window.MouseY);
+    ImGui::Text("Current Time: %f", window.RunTime);
+  gui.End();
+  gui.Render();
+  gui.Draw();
+}
+
+int main(){
+  setup();
+  window.Start(loop);
+}

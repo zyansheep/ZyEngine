7638d8cd2970d14e47e474bcb7a35a58210624cd
zyansheep <zyansheep@github.com>
2020-01-10 17:48:50 -0500

    updated model example

--- examples/models/main.cpp
+++ examples/models/main.cpp
@@ -14,11 +14,14 @@ Object* cube;
 Model sphereModel = ModelGeneration::Icosphere(4);
 Object* sphere;
 
+Model suzanneModel = ModelGeneration::ObjFile("examples/models/suzanne.obj");
+Object* suzanne;
+
 Shader shader;
 
 void setup(){
-  shader = Shader("test/shaders/main.vert", "test/shaders/main.frag");
-  
+  //shader = Shader("test/shaders/main.vert", "test/shaders/main.frag");
+  shader = ShaderGeneration::Color();
   cubeModel.Load();
   cube = new Object(cubeModel.GetVertexArray(), shader);
   
@@ -26,8 +29,13 @@ void setup(){
   sphere = new Object(sphereModel.GetVertexArray(), shader);
   sphere->Translate(glm::vec3(0, 5, 0));
   
+  suzanneModel.Load();
+  suzanne = new Object(suzanneModel.GetVertexArray(), shader);
+  suzanne->Translate(glm::vec3(5, 0, 0));
+  
   world.AddObject(cube);
   world.AddObject(sphere);
+  world.AddObject(suzanne);
   
   controller.Bind(&camera);
   
@@ -39,6 +47,17 @@ void loop(){
   glClearColor(255,255,255,255);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   
+  if(window.GetKey(GLFW_KEY_R)){
+    controller.Unbind();
+    camera = Camera(&window, glm::vec3{0, 0, 8});
+    controller.Bind(&camera);
+  }
+  if(window.GetKey(GLFW_KEY_ESCAPE)){
+    controller.Unbind();
+  }
+  if(window.GetKey(GLFW_KEY_B)){
+    controller.Bind(&camera);
+  }
   controller.Update();
   
   world.Render();
